4b7dc98f3e39787b9f36e09a128dc17f6f2f8e72
Mugiwara No Zaka <30044@ma-web.nl>
2020-03-30 13:11:55 +0200

    2de versie van mn game

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour
+{
+
+	public float move;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			transform.position = new Vector3(0, move, 0);
+			move-= 10;
+		}
+	}
+}
---
+++ Assets/Scripts/Illusion.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Illusion : MonoBehaviour
+{
+
+	public float move;
+	private float posX;
+	private float posZ;
+	private float rotY;
+	// Use this for initialization
+	void Start()
+	{
+		posX = Random.Range(300, -300);
+		posZ = Random.Range(300, -300);
+		rotY = Random.Range(360, -360);
+		transform.Rotate(new Vector3(0, rotY, 0));
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			transform.position = new Vector3(posX, move, posZ);
+			posX -= 5;
+			posZ -= 5;
+			move -= 10;
+		}
+		
+	}
+}
